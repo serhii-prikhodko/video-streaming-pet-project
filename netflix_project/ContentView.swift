a2c38f22cb0909f42b593dd8bf1443e01ab3c3d3
Serhii Prykhodko <v-sprykhodko@expediagroup.com>
2022-01-18 11:27:20 +0200

    Initial Commit

---
+++ netflix_project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  netflix_project
+//
+//  Created by Serhii Prykhodko on 18.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ netflix_project/netflix_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflix_projectApp.swift
+//  netflix_project
+//
+//  Created by Serhii Prykhodko on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct netflix_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
